2526e34df0f43725197264d5244c28b791ec6a5a
up-to-star <2200316527@qq.com>
2025-10-19 01:47:32 +0800

    feat(数据集): 实现模型检查点的保存和加载功能
    添加save_checkpoint和load_checkpoint函数用于保存和恢复模型状态

--- cs336_basics/dataset/utils.py
+++ cs336_basics/dataset/utils.py
@@ -1,6 +1,9 @@
 import numpy as np
 import torch
+from torch import nn
 import numpy.typing as npt
+import os
+from typing import IO, Any, BinaryIO
 
 
 def get_batch(x: npt.NDArray, batch_size: int, context_length: int, device: str):
@@ -15,3 +18,20 @@ def get_batch(x: npt.NDArray, batch_size: int, context_length: int, device: str)
     input_tensor = input_batch.to(device)
     target_tensor = target_batch.to(device)
     return input_tensor, target_tensor
+
+
+def save_checkpoint(model: nn.Module, optimizer: torch.optim.Optimizer, iteration: int, out: str | os.PathLike | BinaryIO | IO[bytes]):
+    checkpoint = {
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'iteration': iteration,
+    }
+    torch.save(checkpoint, out)
+
+
+def load_checkpoint(src: str | os.PathLike | BinaryIO | IO[bytes], model: nn.Module, optimizer: torch.optim.Optimizer):
+    checkpoint = torch.load(src)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    iteration = checkpoint['iteration']
+    return iteration
