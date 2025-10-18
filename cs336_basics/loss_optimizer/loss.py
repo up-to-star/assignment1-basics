1a115a76e3818116bfa96915c8e2256417793cb9
up-to-star <2200316527@qq.com>
2025-10-18 16:16:33 +0800

    feat(optimizer): 添加余弦学习率调度函数并更新测试
    添加余弦学习率调度函数lr_cosine_schedule，支持预热和余弦退火
    更新测试用例run_get_lr_cosine_schedule以使用新函数

--- cs336_basics/loss_optimizer/loss.py
+++ cs336_basics/loss_optimizer/loss.py
@@ -72,6 +72,16 @@ class AdamW(torch.optim.Optimizer):
                 state['t'] = t + 1
         return loss
 
+
+def lr_cosine_schedule(it: int, max_learning_rate: float, min_learning_rate: float, warmup_iters: int, cosine_cycle_iters: int):
+    if it < warmup_iters:
+        return max_learning_rate * it / warmup_iters
+    elif it > cosine_cycle_iters:
+        return min_learning_rate
+    else:
+        return min_learning_rate + (max_learning_rate - min_learning_rate) * (1 + math.cos(math.pi * (it - warmup_iters) / (cosine_cycle_iters - warmup_iters))) / 2
+
+
 if __name__ == '__main__':
     weights = torch.nn.Parameter(5 * torch.randn((10, 10)))
     opt = SGD([weights], lr=1)
